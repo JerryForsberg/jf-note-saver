2caaa94cfc9c890932dc0f1f845d3ff6de57b70e
JerryForsberg <Jerryforsberg8991@gmail.com>
2020-06-18 20:24:27 -0700

    revised some file paths and made new folders

---
+++ Develop/routes/api-routes.js
@@ -0,0 +1,20 @@
+const noteData = require('../public/assets/db/db.json');
+
+module.exports = function (app) {
+    app.get('/api/notes', function (req, res) {
+        res.json(noteData)
+    });
+
+    app.post('/api/notes', function (req, res) {
+        noteData.push(req.body);
+        res.json(true);
+    });
+
+
+    app.delete('api/notes/:id', function (req, res) {
+        if (noteData.id === req.id);
+    });
+
+
+
+}
